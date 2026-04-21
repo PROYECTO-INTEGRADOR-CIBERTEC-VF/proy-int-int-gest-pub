b77cc87826efd3d3775d7226cc309255a6851f37
Kelly Medalit De La Cruz Lopez <delacruzkelly14@gmail.com>
2026-04-20 22:37:48 -0500

    test: proveer ActivatedRoute mock en resolucion detalle

--- transparencia-frontend/src/app/pages/ttaip/ttaip-resolucion-detalle.component.spec.ts
+++ transparencia-frontend/src/app/pages/ttaip/ttaip-resolucion-detalle.component.spec.ts
@@ -1,19 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TtaipResolucionDetalleComponent } from './ttaip-resolucion-detalle.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TtaipResolucionDetalleComponent', () => {
-  let component: TtaipResolucionDetalleComponent ;
-  let fixture: ComponentFixture<TtaipResolucionDetalleComponent >;
+  let component: TtaipResolucionDetalleComponent;
+  let fixture: ComponentFixture<TtaipResolucionDetalleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TtaipResolucionDetalleComponent ],
-    }).compileComponents();
+      imports: [TtaipResolucionDetalleComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: { get: () => 'EXP-TEST-123' } } // Simulamos una URL falsa
+          }
+        }
+      ]
+    })
+      .compileComponents();
 
-    fixture = TestBed.createComponent(TtaipResolucionDetalleComponent );
+    fixture = TestBed.createComponent(TtaipResolucionDetalleComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
